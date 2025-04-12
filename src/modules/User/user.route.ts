3fb099376903cb6a59577a45600bd3483d4664b1
Raiyan109 <kraiyan109@gmail.com>
2025-04-12 15:29:25 +0600

    fileuploadhelper

--- src/modules/User/user.route.ts
+++ src/modules/User/user.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import { FileUploadHelper } from '../../helpers/FileUploadHelper';
 
 
 const router = express.Router();
 
 router.route('/')
-    .post(UserControllers.registerUser)
+    .post(FileUploadHelper.ImageUpload.fields([
+        { name: "admin_profile", maxCount: 1 },
+      ]),UserControllers.registerUser)
 
 export const UserRoutes = router;
