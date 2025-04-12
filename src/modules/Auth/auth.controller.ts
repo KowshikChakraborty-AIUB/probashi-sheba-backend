6916821477dc283d960826ab31224e15741418d3
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-04-12 14:40:34 +0600

    commented auth modules

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -1,40 +1,40 @@
-import httpStatus from 'http-status';
-import { AuthServices } from './auth.service';
-import catchAsync from '../../Utils/catchAsync';
-import { User } from '../User/user.model';
-import sendResponse from '../../Utils/sendResponse';
+// import httpStatus from 'http-status';
+// import { AuthServices } from './auth.service';
+// import catchAsync from '../../Utils/catchAsync';
+// import { User } from '../User/user.model';
+// import sendResponse from '../../Utils/sendResponse';
 
-const loginUser = catchAsync(async (req, res) => {
-    const result = await AuthServices.loginUser(req.body);
-    const { accessToken } = result;
+// const loginUser = catchAsync(async (req, res) => {
+//     const result = await AuthServices.loginUser(req.body);
+//     const { accessToken } = result;
 
-    const userLoginInfo = await User.find({ email: req.body.email })
+//     const userLoginInfo = await User.find({ email: req.body.email })
 
 
-    const { _id, name, email, phone, address, role, profileImg, verified, followers, payment, following } = userLoginInfo[0];
+//     const { _id, name, email, phone, address, role, profileImg, verified, followers, payment, following } = userLoginInfo[0];
 
-    res.status(httpStatus.OK).json({
-        success: true,
-        statusCode: httpStatus.OK,
-        message: 'User logged in successfully',
-        token: accessToken,
-        data: { _id, name, email, phone, address, role, profileImg, verified, payment, followers, following },
-    });
-});
+//     res.status(httpStatus.OK).json({
+//         success: true,
+//         statusCode: httpStatus.OK,
+//         message: 'User logged in successfully',
+//         token: accessToken,
+//         data: { _id, name, email, phone, address, role, profileImg, verified, payment, followers, following },
+//     });
+// });
 
-const changeUserPassword = catchAsync(async (req, res) => {
+// const changeUserPassword = catchAsync(async (req, res) => {
 
-    const result = await AuthServices.changeUserPassword(req.user, req.body);
+//     const result = await AuthServices.changeUserPassword(req.user, req.body);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: result.message,
-        data: result,
-    });
-});
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: result.message,
+//         data: result,
+//     });
+// });
 
-export const AuthControllers = {
-    loginUser,
-    changeUserPassword
-};
+// export const AuthControllers = {
+//     loginUser,
+//     changeUserPassword
+// };
