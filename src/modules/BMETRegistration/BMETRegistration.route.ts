44c6098cf22081fe88bef7cd1cbb63ab93cfafb2
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-04-13 14:22:47 +0600

    added get bmet registration info logic

--- src/modules/BMETRegistration/BMETRegistration.route.ts
+++ src/modules/BMETRegistration/BMETRegistration.route.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { FileUploadHelper } from "../../helpers/FileUploadHelper";
-import { postBMETRegistration, updateBMETRegistration } from "./BMETRegistration.controller";
+import { getBMETRegistration, postBMETRegistration, updateBMETRegistration } from "./BMETRegistration.controller";
 
 const router = express.Router();
 
 // Create, Update, Get BMET
 router
     .route("/")
-    //   .get(findAllCategory)
+    .get(getBMETRegistration)
     .post(
         FileUploadHelper.ImageUpload.fields([
             { name: "passport_image", maxCount: 1 },
