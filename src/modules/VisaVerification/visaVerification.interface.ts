551dd814b1085de9b9e995a98101af4c3a3edbde
Raiyan109 <kraiyan109@gmail.com>
2025-04-19 18:27:59 +0600

    visa verification started

---
+++ src/modules/VisaVerification/visaVerification.interface.ts
@@ -0,0 +1,7 @@
+export interface IVisaVerification {
+    visaVerification_country_name_english?: string;
+    visaVerification_country_name_bangla?: string;
+    visaVerification_country_image?: string;
+    visaVerification_country_image_key?: string;
+    visaVerification_country_link?: string;
+}
